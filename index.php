5cc91e297a941ad72d7b9509291b8944de425bb7
bSchutters <bryan021@hotmail.be>
2023-11-15 14:02:50 +0100

    starting project

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./css/reset.css" />
+  <link rel="stylesheet" href="./css/style.css" />
+
+  <link href="https://cdn.jsdelivr.net/npm/daisyui@4.0.7/dist/full.min.css" rel="stylesheet" type="text/css" />
+  <script src="https://cdn.tailwindcss.com"></script>
+
+  <title>The Dream</title>
+</head>
+
+<body>
+  <div class="navbar bg-base-300 justify-center">
+    <button class="btn btn-ghost text-xl">Currency converter</button>
+  </div>
+
+  <main class="p-5">
+    <div>
+      <form action="" method="post">
+        <div>
+        </div>
+      </form>
+    </div>
+  </main>
+
+  <footer class="footer footer-center p-4 bg-base-300 text-base-content">
+    <aside>
+      <p>Copyright © 2023 - All right reserved by bSchutters</p>
+    </aside>
+  </footer>
+
+
+
+
+
+  <script type="module" src="./js/main.js"></script>
+</body>
+
+</html>
